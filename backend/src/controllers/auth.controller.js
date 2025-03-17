3e96f4dbe0d555f100831d218df8c782ad0fc953
Loay Ahmed <loayahmed304@gmail.com>
2025-03-17 16:42:35 +0200

    add signup route

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,54 @@
-export const signup = (req, res) => {
-  res.send("Signup");
+import bcrypt from "bcryptjs";
+import User from "../models/user.model.js";
+import { generateToken } from "../lib/utils.js";
+
+export const signup = async (req, res) => {
+  const { fullName, email, password } = req.body;
+  if (!fullName || !email || !password) {
+    return res
+      .status(400)
+      .json({ status: "fail", message: "All fields are required" });
+  }
+  try {
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ status: "fail", message: "Password is too short (<6)" });
+    }
+
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({
+        status: "fail",
+        message: "user already exists with this email",
+      });
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      fullName: fullName,
+      email: email,
+      password: hashedPassword,
+    });
+
+    if (newUser) {
+      // generate the jwt token
+      generateToken(newUser._id, res);
+      await newUser.save();
+
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        email: newUser.email,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      res.status(400).json({ status: "fail", message: "Failed to create" });
+    }
+  } catch {
+    res.status(500).json({ status: "fail", message: "Server error" });
+  }
 };
 export const login = (req, res) => {
   res.send("login");
