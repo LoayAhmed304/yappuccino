42716f717a893a6fb3e66d60e9251b36df452ad7
LoayAhmed304 <loayahmed304@gmail.com>
2025-06-22 22:43:50 +0300

    fix(frontend): fix online users count

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -36,7 +36,8 @@ const Sidebar = () => {
             <span className="text-sm">Show online only</span>
           </label>
           <span className="text-xs text-zinc-500">
-            ({onlineUsers.length - 1} online)
+            ({onlineUsers.length - 1 === -1 ? 0 : onlineUsers.length - 1}{" "}
+            online)
           </span>
         </div>
       </div>
