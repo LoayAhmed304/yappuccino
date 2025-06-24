7b0bdc902d5b1a4fd7943826a01dabe2f59649fc
LoayAhmed304 <loayahmed304@gmail.com>
2025-06-24 18:41:54 +0300

    feat: add outgoing call ringtone

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -11,7 +11,28 @@ const HomePage = () => {
   const { selectedUser, isRinging, caller, inCall, isCalling, voiceCall } =
     useChatStore();
   const navigate = useNavigate();
+
   const ringtoneRef = useRef(null);
+  const calltoneRef = useRef(null);
+
+  useEffect(() => {
+    if (!calltoneRef.current) {
+      calltoneRef.current = new Audio("/outgoingtone.mp3");
+      calltoneRef.current.loop = true;
+    }
+    if (isCalling) {
+      calltoneRef.current.play().catch((e) => {
+        console.warn(
+          "Autoplay failed, user interaction required to play sound",
+          e
+        );
+      });
+    } else {
+      calltoneRef.current.pause();
+      calltoneRef.current.currentTime = 0;
+    }
+  }, [isCalling]);
+
   useEffect(() => {
     if (!ringtoneRef.current) {
       ringtoneRef.current = new Audio("/ringtone.mp3");
