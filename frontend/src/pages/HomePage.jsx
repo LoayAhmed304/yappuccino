42a69b1288a95652d2f186f6200bed914860c252
Loay Ahmed <loayahmed304@gmail.com>
2025-07-13 20:35:47 +0300

    perf: introduce Nginx and optimize codebase for production
    * feat(ui): add informative text in homepage to acknowledge encryption
    
    * feat(ui): add username warning in signup page
    
    * chore: adjust project structure to match production environment
    
    * fix: fix docker compose file to work for development and production
    
    * perf: remove useless named volume in production docker compose file

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -54,46 +54,6 @@ const HomePage = () => {
     }
   }, [isRinging]);
 
-  const ringtoneRef = useRef(null);
-  const calltoneRef = useRef(null);
-
-  useEffect(() => {
-    if (!calltoneRef.current) {
-      calltoneRef.current = new Audio("/outgoingtone.mp3");
-      calltoneRef.current.loop = true;
-    }
-    if (isCalling) {
-      calltoneRef.current.play().catch((e) => {
-        console.warn(
-          "Autoplay failed, user interaction required to play sound",
-          e
-        );
-      });
-    } else {
-      calltoneRef.current.pause();
-      calltoneRef.current.currentTime = 0;
-    }
-  }, [isCalling]);
-
-  useEffect(() => {
-    if (!ringtoneRef.current) {
-      ringtoneRef.current = new Audio("/ringtone.mp3");
-      ringtoneRef.current.loop = true;
-    }
-
-    if (isRinging) {
-      ringtoneRef.current.play().catch((e) => {
-        console.warn(
-          "Autoplay failed, user interaction required to play sound",
-          e
-        );
-      });
-    } else {
-      ringtoneRef.current.pause();
-      ringtoneRef.current.currentTime = 0;
-    }
-  }, [isRinging]);
-
   useEffect(() => {
     if (inCall) {
       navigate("/call");
